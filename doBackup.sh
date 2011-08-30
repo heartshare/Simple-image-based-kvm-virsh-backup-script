07ac64a6afa15c0bd7e314301b645a9fad1ff0d3
Tyler Stafford <tyler.stafford@gmail.com>
2011-08-30 12:52:28 -0700

    added argument handling added -z argument to enable gzip compression of the image

--- doBackup.sh
+++ doBackup.sh
@@ -1,23 +1,77 @@
 #!/bin/bash
-dd=/bin/dd
+#TODO: these variables should be set at the command line
+domain=""
+blockdev=""
+image=""
+
+dd="/bin/dd"
 virsh="/usr/bin/virsh"
-domain="virsh domain name here"
-blockdev=/path/to/disk/image/or/block/device
-image=/path/to/target/file
+gzip="/bin/gzip"
 isRunning="0"
 wasRunning="1"
 start=`date +%s`
 blockSize="1M"
+compress="0"
 
-function quit {
+function Quit {
+	if [ -n "$*" ] ; then 
+		echo "$*";
+		exit
+	fi
+
 	if [ "$wasRunning" -eq "0" ]; then
 		$virsh start "$domain" > /dev/null 2>&1
 	fi
-	echo "$1"
 	echo "Operation took $((`date +%s` - $start)) seconds."
 	exit
 }
 
+function Usage {
+	echo "Usage: `basename $0` -i source -o destination -n guestName [-z]"
+	echo "-i file/device to backup"
+	echo "-o path of output file"
+	echo "-n name of guest"
+	echo "-z compress the archive using gzip"
+	Quit
+}
+
+function CheckFiles {
+	#check if required variables are set (not sure if there's a better way to do this)
+	if [ -z "$domain" ]; then
+		Usage
+	fi
+	if [ -z "$blockdev" ]; then
+		Usage
+	fi
+	if [ -z "$image" ]; then
+		Usage
+	fi
+
+	#check if the files exist
+	if [ ! -e "$blockdev" ]; then
+		Quit "the source file does not exist";
+	fi
+	if [ -e "$image" ]; then
+		Quit "$image already exists";
+	fi
+}
+
+
+#get the options
+while getopts "i:o:n:z" Option
+do
+  case $Option in
+    i)blockdev="$OPTARG";;
+    o)image="$OPTARG";;
+    n)domain="$OPTARG";;
+    z)compress="0";;
+    ?)Usage;;
+  esac
+done
+
+CheckFiles
+
+#start the operation
 until [ "$isRunning" -eq "1" ]; do
 
 	$virsh list | grep -w "$domain" > /dev/null 2>&1 
@@ -28,14 +82,13 @@ until [ "$isRunning" -eq "1" ]; do
 		wasRunning="0"
 		$virsh shutdown "$domain" > /dev/null 2>&1
 	else
-		echo "Copying $blockdev to $image"
-		$dd if="$blockdev" of="$image" bs="$blockSize"
-		if [ "$?" -eq "0" ]; then
-			quit "Success!"
+		if [ -r "$blockdev" ] ; then
+			echo "Copying $blockdev to $image"
+			$dd if="$blockdev" bs="$blockSize" | $gzip > $image
+			Quit "Success!"
 		else
-			quit "FAIL"
+			Quit "You do not have read access for $blockdev"
 		fi
 	fi
-	
-	sleep 1
+	sleep 60
 done
